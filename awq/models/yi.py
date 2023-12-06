11c24206467d14ab756a6c8fc12d962aba91f7a2
jamesdborin <james.dborin@gmail.com>
2023-12-06 19:45:51 +0000

    remove broken import

--- awq/models/yi.py
+++ awq/models/yi.py
@@ -1,11 +1,6 @@
 import tqdm
 from typing import List, Tuple
 from .base import BaseAWQForCausalLM
-from awq.utils.fused_utils import fuse_qkv
-from awq.modules.fused.block import LlamaLikeBlock
-from awq.modules.fused.model import LlamaLikeModel
-from awq.modules.fused.mlp import QuantFusedMLP
-from awq.modules.fused.norm import FasterTransformerRMSNorm
 
 class YiAWQForCausalLM(BaseAWQForCausalLM):
     layer_type = "YiDecoderLayer"
